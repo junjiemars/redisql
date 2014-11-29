72d9b0e82d40b68052174b19d547d0c5b4b22496
南山 <junjiemars@gmail.com>
2014-11-29 09:31:12 +0800

    init clojure

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :main ^:skip-aot jql.core
+  ;:main ^:skip-aot jql.core
+  :main jql.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
