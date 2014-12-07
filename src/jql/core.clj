028e20578a807de93501062d17c7750649f068b0
南山 <junjiemars@gmail.com>
2014-12-07 13:06:44 +0800

    dsl tutorial

--- src/jql/core.clj
+++ src/jql/core.clj
@@ -1,7 +1,50 @@
 (ns jql.core
   (:gen-class))
 
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(derive ::bash ::common)
+(derive ::batch ::common)
+
+(defmulti emit
+  (fn [form]
+    [*current-implementation* (class form)]))
+
+(def ^{:dynamic true}
+  "The current script language implementation to generate"
+  *current-implementation*)
+
+(defmethod emit
+  [::bash clojure.lang.PersistentList]
+  [form]
+  (case (name (first form))
+    "println" (str "echo " (second form))
+    nil))
+
+(defmethod emit
+  [::batch clojure.lang.PersistentList]
+  [form]
+  (case (name (first form))
+    "println" (str "ECHO " (second form))
+    nil))
+
+(defmethod emit
+  [::common java.lang.String]
+  [form]
+  form)
+
+(defmethod emit
+  [::common java.lang.Long]
+  [form]
+  (str form))
+
+(defmethod emit
+  [::common java.lang.Double]
+  [form]
+  (str form))
+
+
