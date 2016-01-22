b4331263a695f89bfa5ef8af7d9913091c9737de
南山竹 <junjiemars@gmail.com>
2016-01-22 23:58:20 +0800

    simple insta parser test from bnf file

--- src/redisql/core.clj
+++ src/redisql/core.clj
@@ -2,7 +2,8 @@
   (:require [clojure.tools.cli :refer [parse-opts]]
             [clojure.string :as string]
             [clojure.tools.logging :as log]
-            [clojure.java.io :as io])
+            [clojure.java.io :as io]
+            [instaparse.core :as i])
   (:gen-class))
 
 (defn exit
@@ -33,9 +34,11 @@
                           args \newline errors))
       (:eval options)
       (let [input (:eval options)
-            bnf (:file options)]
+            bnf (slurp (:file options))]
         (println input)
         (println bnf)
+        (println "------------------")
+        ((i/parser bnf) input)
         (log/debug input))
 
       :else (exit 1 summary))))
