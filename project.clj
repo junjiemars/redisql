5732f4aff18ce2fc8c5eda82230634b60e971d28
南山竹 <junjiemars@gmail.com>
2016-02-29 10:26:27 +0800

    refine lien's project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject redisql "0.1.0"
-  :description "redis query language like sql"
+  :description "Play Redis as a rational store via SQL"
   :url "https://github.com/junjiemars/redisql"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
